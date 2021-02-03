d57a4453d6d750f8f8acba4de5defb7b170e517a
Denzil Rogers <21Denzil.Rogers@boiseschools.net>
2021-02-03 12:57:03 -0700

    Initial commit

---
+++ ZombieFighter.py
@@ -0,0 +1,34 @@
+import pygame, sys
+
+from pygame.locals import *
+from time import sleep
+from random import randint
+
+pygame.init()
+
+FPS = 60 # frames per second to update the screen
+fpsClock = pygame.time.Clock()
+
+DISPLAYSURF = pygame.display.set_mode((500, 600), pygame.RESIZABLE)
+
+pygame.display.set_caption("Zombie Fighter")
+
+
+class Zombie(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect()
+
+
+def main():
+    return 1
